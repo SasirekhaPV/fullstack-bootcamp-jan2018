96aaea7cfa5bdbd9967bbc02ac3ca58f17141797
Abhishek Rathore <rathore.abhishek87@gmail.com>
2018-02-16 21:40:20 +0530

    routing params

--- Day13/js/app.js
+++ Day13/js/app.js
@@ -2,6 +2,7 @@ angular.module("eKart",['ngRoute'])
         .config(config)
         .controller('productCtrl',productCtrl)
         .controller('aboutCtrl',aboutCtrl)
+        .controller('detailCtrl',detailCtrl)
         .factory('products',products)
 
 
@@ -13,11 +14,22 @@ function config($routeProvider){
         controller:'productCtrl',
         controllerAs:'product'
     })
+    .when("/category/:category/product/:product",{
+        templateUrl:'partials/product.html',
+        controller:'detailCtrl',
+        controllerAs:'detail'
+    })
+    .when("/category/:category/:brand",{
+        templateUrl:'partials/product.html',
+        controller:'productCtrl',
+        controllerAs:'product'
+    })
     .when("/about",{
         templateUrl:'partials/about.html',
         controller:'aboutCtrl',
         controllerAs:'about'
     })
+    .otherwise({redirectTo:"/category/0"})
 
 
 }
@@ -27,11 +39,25 @@ function aboutCtrl(products){
     console.log("about",products)
 }
 
+function detailCtrl(products,$routeParams){
+    console.log($routeParams)
+    var detail = this;
+    var category = products[parseInt($routeParams.category)]
+    console.log(category)
+
+    for(var i=0;i<category.products.length;i++){
+        if($routeParams.product==category.products[i].name){
+            detail.item = category.products[i]
+        }
+    }
+}
+
 function productCtrl(products,$routeParams){
    var product = this;
 
    console.log($routeParams.category)
 
+   product.category = $routeParams.category;
    product.list = products;
    product.items = products[parseInt($routeParams.category)].products;
     console.log(products)
@@ -53,15 +79,15 @@ function products(){
     return [
         {name:"shoes",
         products:[
-            {name:"Nike Air",brand:"Nike",price:20.00,desc:"xyz",rating:4,image:"demo.png"},
+            {name:"Nike Air",brand:"Nike",price:20.00,desc:"xyz",rating:4,image:"demo.png",tech:"jakshjdshjdshjsdjhjh"},
             {name:"Reebok Air",brand:"Reebok",price:30.00,desc:"xyz",rating:3,image:"demo.png"},
             {name:"Adidas Ultraboost",brand:"Adidas",price:50.00,desc:"xyz",rating:5,image:"demo.png"},
             {name:"Woodland Rockers",brand:"Woodland",price:40.00,desc:"xyz",rating:1,image:"demo.png"}
         ]},
         {name:"phones",
         products:[
-            {name:"Samsung ",brand:"Nike",price:20.00,desc:"xyz",rating:4,image:"demo.png"},
-            {name:"Nokia ",brand:"Reebok",price:30.00,desc:"xyz",rating:3,image:"demo.png"},
+            {name:"Samsung",brand:"Nike",price:20.00,desc:"xyz",rating:4,image:"demo.png"},
+            {name:"Nokia",brand:"Reebok",price:30.00,desc:"xyz",rating:3,image:"demo.png"},
             {name:"Apple Ultraboost",brand:"Adidas",price:50.00,desc:"xyz",rating:5,image:"demo.png"},
             {name:"MI Rockers",brand:"Woodland",price:40.00,desc:"xyz",rating:1,image:"demo.png"}
         ]},
